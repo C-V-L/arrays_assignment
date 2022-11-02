87f775c3926cc4b1b11310ae15f2216041b6925d
Conner Van Loan <cdvanloan@gmail.com>
2022-11-02 16:05:24 -0600

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,6 @@
+fav_artists = ["Kendrick Lamar", "Led Zepplin", "Andre 3000" , 'Fleet Foxes']
+album_ratings = [10, 9, 8, 7, 6]
+current_artist = [true, false, true, true]
+avg_ticket_price = [65.7, 89.3, 23.7, 45.1]
+
+puts current_artist
